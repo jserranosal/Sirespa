aac2937ab10a923b11d3587799d6a6a313ee1528
jserranosal <jserranosal@hotmail.com>
2017-02-20 17:18:17 -0600

    Se cambio web.config para probar
    asdasdadasdsa

---
+++ SP.General.Catalogo/SP.General.AccesoDatos/Clientes/ClientesConstantesBD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.General.AccesoDatos.Clientes
+{
+    public static class ClientesConstantesBD
+    {
+        public static string strConexion = "SqlCnx";
+
+        public static string strCLIENTES_OBTENER = "USP_CLIENTES_OBTENER";
+        public static string strCLIENTES_INSERTAR = "USP_CLIENTES_OBTENER";
+        public static string strCLIENTES_ACTUALIZAR = "USP_CLIENTES_ACTUALIZAR";
+        public static string strCLIENTES_ELIMINAR = "USP_CLIENTES_ELIMINAR";
+
+        public static string strKEYCLI = "KEYCLI";
+        public static string strKEYTCP = "KEYTCP";
+        public static string strTPCDES = "TPCDES";      //
+        public static string strRFCCLI = "RFCCLI";
+        public static string strNOMCLI = "NOMCLI";
+        public static string strAPEPAT = "APEPAT";
+        public static string strAPEMAT = "APEMAT";
+        public static string strDOMCLI = "DOMCLI";
+        public static string strNUMEXT = "NUMEXT";
+        public static string strNUMINT = "NUMINT";
+        public static string strCOLONI = "COLONI";
+        public static string strCODPOS = "CODPOS";
+        public static string strNOMPAI = "NOMPAI";
+        public static string strNOMEDO = "NOMEDO";
+        public static string strNOMCIU = "NOMCIU";
+        public static string strCLIFUC = "CLIFUC";
+        public static string strCLIFAL = "CLIFAL";
+    }
+}
